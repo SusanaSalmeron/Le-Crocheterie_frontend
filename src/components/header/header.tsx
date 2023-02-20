44d13aeacf6170d6dd9d7b7a0ee97a2ca6697f48
SusanaSalmeron <exdream76@gmail.com>
2023-02-20 11:54:20 +0100

    header structure created

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,38 +5,40 @@ import styles from './header.module.css';
 
 interface HeaderProps { }
 
-const Header: FC<HeaderProps> = () => (
-  <div className={styles.Header} data-testid="Header">
-    <nav className={styles.navbar}>
-      <div className={styles.container}>
-        <div className={styles.elements}>
-          <ul className={styles.menu}>
-            <li>
-              <Link to="home">
-                <i className="fa-solid fa-house" />
-              </Link>
-            </li>
-            <li>
-              <Link to="home">products</Link>
-            </li>
-            <li>
-              <Link to="home">about</Link>
-            </li>
-            <li>
-              <Link to="home">contact</Link>
-            </li>
-          </ul>
+const Header: FC<HeaderProps> = () => {
+  return (
+    <div className={styles.Header} data-testid="Header">
+      <nav className={styles.navbar}>
+        <div className={styles.container}>
+          <div className={styles.elements}>
+            <ul className={styles.menu}>
+              <li>
+                <Link to="home">
+                  <i className="fa-solid fa-house" />
+                </Link>
+              </li>
+              <li>
+                <Link to="home">products</Link>
+              </li>
+              <li>
+                <Link to="home">about</Link>
+              </li>
+              <li>
+                <Link to="home">contact</Link>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.logo}>
+            <p>logo</p>
+          </div>
+          <div className={styles.search}>
+            <label>Search</label>
+            <input type='text' name='search' />
+          </div>
         </div>
-        <div className={styles.logo}>
-          <p>logo</p>
-        </div>
-        <div className={styles.search}>
-          <label>Search</label>
-          <input type='text' name='search' />
-        </div>
-      </div>
-    </nav>
-  </div>
-);
+      </nav>
+    </div>
+  )
+};
 
 export default Header;
