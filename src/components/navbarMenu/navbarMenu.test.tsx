f255ee1748a4d059c7b4ad9ec65f2d27347d4d88
SusanaSalmeron <exdream76@gmail.com>
2023-02-20 18:25:00 +0100

    fixing test

--- src/components/navbarMenu/navbarMenu.test.tsx
+++ src/components/navbarMenu/navbarMenu.test.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import NavbarMenu from './navbarMenu';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('<NavbarMenu />', () => {
   test('it should mount', () => {
-    render(<NavbarMenu />);
+    render(
+      <BrowserRouter>
+        <NavbarMenu />
+      </BrowserRouter>);
 
     const navbarMenu = screen.getByTestId('NavbarMenu');
 
