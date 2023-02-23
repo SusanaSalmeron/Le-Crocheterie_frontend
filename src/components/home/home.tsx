4fd8b58596b08714e4242c91fad0444f86f5065f
SusanaSalmeron <exdream76@gmail.com>
2023-02-23 12:35:54 +0100

    section 100% created

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -6,9 +6,33 @@ import styles from './home.module.css';
 interface HomeProps { }
 
 const Home: FC<HomeProps> = () => (
-  <div className={styles.Home} data-testid="Home">
+  <div className={styles.home} data-testid="home">
     <div className={styles.carousel}>
       <HomeCarousel />
+    </div>
+    <div className={styles.section}>
+      <section>
+        <h3>100%
+        </h3>
+        <h4>handmande</h4>
+      </section>
+      <section>
+        <h3>
+          100%
+        </h3>
+        <h4>made with love</h4>
+      </section>
+      <section>
+        <h3>
+          100%
+        </h3>
+        <h4>
+          beneficent
+        </h4>
+      </section>
+    </div>
+    <div className={styles.types}>
+
     </div>
   </div>
 );
