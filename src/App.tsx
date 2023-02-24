e701dfd3bb5aceaad2045325e92154b745895698
SusanaSalmeron <exdream76@gmail.com>
2023-02-24 12:08:22 +0100

    basic footer created

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/home/home';
 import Header from './components/header/header'
+import Footer from './components/footer/footer';
 
 
 function App() {
   return (
-    <div>
+    <div className='App'>
       <Header />
       <Routes>
         <Route path="/home" element={<Home />}></Route>
       </Routes>
+      <div className='footer'>
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+import styles from './footer.module.css';
+
+interface FooterProps { }
+
+const Footer: FC<FooterProps> = () => {
+  const year: number = new Date().getFullYear();
+  return (<div className={styles.footer} data-testid="footer">
+    <footer>{`Copyright © Le Crocheterie ${year}`}</footer>
+  </div>
+  )
+};
+
+export default Footer;
