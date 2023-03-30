efbd473154b33e0db71792445147a5c6420f7f82
SusanaSalmeron <exdream76@gmail.com>
2023-03-30 14:01:38 +0200

    productDetails test updated

--- src/components/productDetails/productDetails.test.tsx
+++ src/components/productDetails/productDetails.test.tsx
@@ -12,8 +12,10 @@ jest.mock('react-router-dom', () => ({
 
 describe('<ProductDetails />', () => {
   const productService = require('../../services/productsService')
+  const catalogService = require('../../services/catalogService')
   test('it should show product details correctly', async () => {
     jest.spyOn(productService, 'getProductBy').mockResolvedValueOnce({ id: 5, name: "Pochi", material: "cotton", colors: ["pink"], description: "asdfghj", price: 15 })
+    jest.spyOn(catalogService, 'getCatalogs').mockResolvedValue({ "colors": ["pink", "blue"], "materials": ["cotton"], "sizes": { "l": 25, "m": 15, "s": 10 } })
 
     render(
       <BrowserRouter>
@@ -26,7 +28,9 @@ describe('<ProductDetails />', () => {
     expect(img).toHaveAttribute('src', 'https://d1ccwz5tu7strp.cloudfront.net/5/main.jpg')
     const name = screen.getAllByRole('heading')
     expect(name).toHaveLength(2)
-    expect(name[1]).toHaveTextContent('15€ - 30€')
+    await waitFor(() => {
+      expect(name[1]).toHaveTextContent('10€ - 25€')
+    })
     await waitFor(() => {
       expect(name[0]).toHaveTextContent('Pochi')
     })
